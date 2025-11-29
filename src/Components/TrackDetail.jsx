60bc66e27ef545bce7d0b94a4b8c7e0598ebceef
lav6301 <lavanyakf@gmail.com>
2025-11-29 16:03:24 +0530

    f create the trackdetail frontend code

---
+++ src/Components/TrackDetail.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./navbar";
+import PageHeader from "./pageHeader";
+import ServiceIcon from "./servicesicon";
+import Footer from "./footer";
+
+function TrackDetail(){
+    return(
+        <>
+        <Navbar />
+        
+        </>
+    )
+}
+
+export default TrackDetail
